5c6ab4e42b9e3a971cb566e1787239f887428bcb
MP <pepper@inbox.ru>
2021-04-06 13:17:20 +0300

    add solution for task 2001

---
+++ src/com/mpetrov/javaschool/hw/lesson1/Task2001.java
@@ -0,0 +1,14 @@
+package com.mpetrov.javaschool.hw.lesson1;
+
+import java.util.Scanner;
+
+public class Task2001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = scanner.nextInt();
+        sum += scanner.nextInt();
+
+        System.out.println(sum);
+    }
+}

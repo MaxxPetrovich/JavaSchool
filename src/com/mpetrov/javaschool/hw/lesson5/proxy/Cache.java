26d01dfa534b1098084b77636d260351052a8c48
MP <pepper@inbox.ru>
2021-04-26 15:17:10 +0300

    add caching and logging proxy

---
+++ src/com/mpetrov/javaschool/hw/lesson5/proxy/Cache.java
@@ -0,0 +1,11 @@
+package com.mpetrov.javaschool.hw.lesson5.proxy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Cache {
+}
